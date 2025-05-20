2add091904431db3e48b5d65f4092e2a19ab81df
Dnayte <129336192+Dnayte@users.noreply.github.com>
2025-05-20 09:07:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidades',
+    'Qual é o único alimento que não estraga?',
+    'O mel. Arqueólogos já encontraram potes de mel comestível em tumbas egípcias com mais de 3 mil anos!'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Por que os gatos ronronam?',
+    'Os gatos ronronam para expressar bem-estar, mas também podem ronronar quando estão com medo ou dor — acredita-se que o som ajude na auto-cura, pois as vibrações têm efeito calmante e regenerativo.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual foi o menor país que já existiu?',
+    'Sealand, uma antiga plataforma militar no Mar do Norte, com apenas 550 m², declarou independência em 1967. Embora não seja reconhecido oficialmente, é um dos menores territórios autodeclarados do mundo.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Por que o céu é azul?',
+    'A luz do sol se espalha ao entrar na atmosfera. O azul é desviado com mais facilidade que outras cores por causa do seu comprimento de onda mais curto — por isso, vemos o céu azul durante o dia.'
+)
